428583f434fa900af7aefcd6bfb7278aa43f47cf
Dustin K <miduku11@gmail.com>
2015-12-14 20:16:41 +0100

    now the earth turns correctly from ISS position, with bearing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-(function ($, window, document, undefined) {
+(function ($, d3, window, document, undefined) {
 
   'use strict';
 
@@ -15,14 +15,18 @@ var width = windowWidth,
 
 // globe
 var globe = {type: 'Sphere'},
+    circle = {type: "GeometryCollection", geometries: 'circles'},
     velocity = 0.005,
+    // radius = Math.sqrt(width*height)/3,
     radius = Math.sqrt(width*height)/2,
-    scale = radius;
+    scale = radius,
+    distance = 2;
 
 // colors
 var color = {
   bright: '#fff',
-  dark: '#263238'
+  darkest: '#263238',
+  dark: '#999'
 };
 
 // JSONP data stuff
@@ -30,6 +34,10 @@ var dataISS = null,
     urlISS = 'http://api.open-notify.org/iss-now.json?callback=?',
     latISS = null,
     lonISS = null,
+    latISSPast = 0,
+    lonISSPast = 0,
+    rotationISS = 90,
+    rotationISSPast = 0,
     tick = 0;
 
 
@@ -38,15 +46,27 @@ var canvas = d3.select('.draw')
   .append('canvas')
   .attr('width', width)
   .attr('height', height)
-  .attr('class', 'globe');
+  .attr('class', 'draw-globe');
 
 var context = canvas.node().getContext('2d');
 
 
+// // choose geo projection type
+// var projection = d3.geo.satellite()
+//   .distance(distance)
+//   .scale(scale*2)
+//   .tilt(40)
+//   // .center([0, -7])
+//   // .translate([width/2, height/2])
+//   // .translate([width/2, height + height/8])
+//   .clipAngle(Math.acos(1 / distance) * 180 / Math.PI - 1e-6)
+//   .precision(0.5);
+
 // choose geo projection type
 var projection = d3.geo.orthographic()
-  // .translate([width/2, height/2])
-  .translate([width/2, height + height/6])
+  // .center([50,50])
+  .translate([width/2, height/2])
+  // .translate([width/2, height + height/6])
   .scale(scale)
   .clipAngle(90)
   .precision(0.5);
@@ -62,6 +82,8 @@ d3.json('assets/json/world-110m.json', function(error, dataWorld) {
       throw error;
     }
 
+    getDataISS();
+
     var land = topojson.feature(dataWorld, dataWorld.objects.land);
     var borders = topojson.mesh(dataWorld, dataWorld.objects.countries, function(a, b) { return a != b; });
 
@@ -71,55 +93,96 @@ d3.json('assets/json/world-110m.json', function(error, dataWorld) {
     // Project the data and rotate, too.
     setInterval(function() {
       console.log('update sessionStorage from open notify API');
+      // console.log(lonISSPast);
+      latISSPast = latISS;
+      lonISSPast = lonISS;
+      rotationISSPast = rotationISS;
+      // console.log(lonISS);
+      getDataISS();
 
-      $.getJSON(urlISS, function(storedData) {
-        sessionStorage.setItem('storedData', JSON.stringify(storedData));
-      });
+      // var geoCircle = d3.geo.circle()
+      //   .origin([lonISS, latISS])
+      //   .angle(500);
+      // var geoCircles = [geoCircle()];
 
-      // get data from local storage
-      dataISS = JSON.parse(sessionStorage.getItem('storedData'));
-        latISS = dataISS.iss_position.latitude;
-        lonISS = dataISS.iss_position.longitude;
 
-      console.log(dataISS);
+      // bearing
+      // http://www.movable-type.co.uk/scripts/latlong.html
+      // https://github.com/chrisveness/geodesy
+      var posISS = new LatLon(latISS, lonISS);
+      var posISSPast = new LatLon(latISSPast, lonISSPast);
+      var bearing = posISS.bearingTo(posISSPast);
+      console.log('LatLon degrees: ' + latISS + ', ' + lonISS);
+      console.log('LatLon: ' + posISS);
+      console.log('LatLon past: ' + posISSPast);
+      console.log('Bearing: ' + bearing);
 
-      projection.rotate([lonISS, latISS, 0]); // test rotation
-      // projection.rotate([-velocity * elapsed, 0, 90]); // test rotation
 
-      // don't draw rectangle
-      context.clearRect(0, 0, width, height);
 
-      // outline and fill globe
-      context.beginPath();
-      geoPath(globe);
-      context.fillStyle = color.dark;
-      context.fill();
-      context.lineWidth = 2;
-      context.strokeStyle = color.dark;
-      context.stroke();
 
-      // fill landmass
-      context.beginPath();
-      geoPath(land);
-      context.fillStyle = color.bright;
-      context.fill();
-      context.lineWidth = 1;
-      context.strokeStyle = color.dark;
-      context.stroke();
+      // d3.timer(function(elapsed) {
+      //   d3.transition()
+      //     .duration(4999)
+      //     .ease('linear')
+      //     .tween('rotate', function () {
+            // projection.rotate([52.5167, 13.3833, 0]); // -> Berlin Coords
+            projection.rotate([lonISS, latISS, bearing]); // test rotation
+            // projection.rotate([-velocity * elapsed, 0, 90]); // test rotation
 
-      // draw lines of country borders
-      context.beginPath();
-      geoPath(borders);
-      context.lineWidth = 0.5;
-      context.strokeStyle = color.dark;
-      context.stroke();
-    }, 5000);
+            // don't draw rectangle
+            context.clearRect(0, 0, width, height);
 
-    // d3.timer(function(elapsed) {
+            // outline and fill globe
+            context.beginPath();
+            geoPath(globe);
+            context.fillStyle = color.dark;
+            context.fill();
+            context.lineWidth = 2;
+            context.strokeStyle = color.dark;
+            context.stroke();
 
-    // });
+            // fill landmass
+            context.beginPath();
+            geoPath(land);
+            context.fillStyle = color.bright;
+            context.fill();
+            context.lineWidth = 1;
+            context.strokeStyle = color.dark;
+            context.stroke();
+
+            // draw lines of country borders
+            context.beginPath();
+            geoPath(borders);
+            context.lineWidth = 0.5;
+            context.strokeStyle = color.dark;
+            context.stroke();
+
+            // context.beginPath();
+            // path({type: "GeometryCollection", geometries: geoCircles});
+            // context.fillStyle = color.dark;
+            // context.fill();
+            // context.lineWidth = 0.5;
+            // context.strokeStyle = color.dark;
+            // context.stroke();
+
+      //   }); // END transition
+      // }); // END timer
+    }, 5000); // END setInterval
+
 });
 
-// console.log(dataISS);
+function getDataISS () {
+  $.getJSON(urlISS, function(storedData) {
+    sessionStorage.setItem('storedData', JSON.stringify(storedData));
+  });
 
-})(jQuery, window, document);
+  // get data from local storage
+  dataISS = JSON.parse(sessionStorage.getItem('storedData'));
+      lonISS = dataISS.iss_position.longitude * -1;
+      latISS = dataISS.iss_position.latitude * -1;
+    // lonISS = -121 * -1;
+    // latISS = 35.5 * -1;
+}
+
+
+})(jQuery, d3, window, document);
